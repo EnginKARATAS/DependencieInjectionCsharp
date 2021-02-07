4e7b35d650422acaa7e809cfd6e60377368cb3f3
Engin Karataş <enginkaratas99@gmail.com>
2021-02-07 09:50:57 +0300

    Initial commit

---
+++ DependencieInjectionCsharp/ICreditManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencieInjectionCsharp
+{
+    interface ICreditManager
+    {
+        public void BasvuruYap();
+    }
+}
